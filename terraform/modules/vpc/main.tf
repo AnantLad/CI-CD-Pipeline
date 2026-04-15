db623067eeb98d4bafda8bc527c07742f5c66f58
AnantLad <anantlad018@gmail.com>
2026-04-15 17:08:44 +0530

    error solved of vpc

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -62,6 +62,7 @@ resource "aws_route_table" "public" {
 
     route {
         cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
      }
     tags = {
          Name = "${var.environment}-public-rt"
@@ -71,7 +72,7 @@ resource "aws_route_table" "private" {
     vpc_id = aws_vpc.main.id
 
     route {
-        cidr_block = "0.0.0/0"
+        cidr_block = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.nat.id
      }
     tags = {
